a743e4aacff65ca1abe32575fcc5b02a2bfd4757
Colin C Cook <colinccook@gmail.com>
2025-07-20 07:07:13 +0100

    added integration tests

---
+++ ColinCCook.AppHost.AppHost.IntegrationTests/AspireRunServiceTests.cs
@@ -0,0 +1,48 @@
+namespace ColinCCook.AppHost.AppHost.IntegrationTests;
+
+public class AspireRunServiceTests
+{
+    [Test]
+    public async Task CallingViaDirectRequest_Should_ReturnAccepted()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ColinCCook_AppHost_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("aspirerunservice");
+        await resourceNotificationService.WaitForResourceAsync("aspirerunservice", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/direct-request");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Accepted));
+    }
+
+    [Test]
+    public async Task CallingViaContainer_Should_ReturnCreated()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ColinCCook_AppHost_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("aspirerunservice");
+        await resourceNotificationService.WaitForResourceAsync("aspirerunservice", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("/via-container");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+    }
+}
